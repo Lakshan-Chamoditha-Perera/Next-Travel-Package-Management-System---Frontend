3aaeb8bcfb094e3ecbb68541488a275e8dc7d88d
Shan <perera.alc2000@gmail.com>
2023-10-21 20:33:01 +0530

    guide get all

--- script/guide/model/GuideModel.js
+++ script/guide/model/GuideModel.js
@@ -14,3 +14,22 @@ export function getLastGuideId(){
         }
     )
 }
+
+
+export function getAll(){
+    return new Promise((resolve,reject)=>{
+            let settings = {
+                "url": "http://localhost:8089/api/v1/guide/getAll",
+                "method": "GET",
+                "timeout": 0,
+            };
+            $.ajax(settings).done(function (response, textStatus, jqXHR) {
+                console.log("success")
+                resolve(response);
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                console.log(errorThrown)
+                reject(errorThrown);
+            });
+        }
+    )
+}
