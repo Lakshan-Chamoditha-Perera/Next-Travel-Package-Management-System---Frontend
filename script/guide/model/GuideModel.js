78f9bf029c9a65222dde2073cbfd3fb16a8e4625
Shan <perera.alc2000@gmail.com>
2023-10-21 20:33:20 +0530

    guide get all

--- script/guide/model/GuideModel.js
+++ script/guide/model/GuideModel.js
@@ -1,35 +1,28 @@
-export function getLastGuideId(){
-    return new Promise((resolve,reject)=>{
-            let settings = {
-                "url": "http://localhost:8089/api/v1/guide/get/lastId",
-                "method": "GET",
-                "timeout": 0,
-            };
-            $.ajax(settings).done(function (response, textStatus, jqXHR) {
-                resolve(response);
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                console.log(errorThrown)
-                reject(errorThrown);
-            });
-        }
-    )
+export function getLastGuideId() {
+    return new Promise((resolve, reject) => {
+        let settings = {
+            "url": "http://localhost:8089/api/v1/guide/get/lastId", "method": "GET", "timeout": 0,
+        };
+        $.ajax(settings).done(function (response, textStatus, jqXHR) {
+            resolve(response);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown)
+            reject(errorThrown);
+        });
+    })
 }
 
-
-export function getAll(){
-    return new Promise((resolve,reject)=>{
-            let settings = {
-                "url": "http://localhost:8089/api/v1/guide/getAll",
-                "method": "GET",
-                "timeout": 0,
-            };
-            $.ajax(settings).done(function (response, textStatus, jqXHR) {
-                console.log("success")
-                resolve(response);
-            }).fail(function (jqXHR, textStatus, errorThrown) {
-                console.log(errorThrown)
-                reject(errorThrown);
-            });
-        }
-    )
+export function getAll() {
+    return new Promise((resolve, reject) => {
+        let settings = {
+            "url": "http://localhost:8089/api/v1/guide/getAll", "method": "GET", "timeout": 0,
+        };
+        $.ajax(settings).done(function (response, textStatus, jqXHR) {
+            console.log("success")
+            resolve(response);
+        }).fail(function (jqXHR, textStatus, errorThrown) {
+            console.log(errorThrown)
+            reject(errorThrown);
+        });
+    })
 }
