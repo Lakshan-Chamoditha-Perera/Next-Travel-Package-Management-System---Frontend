2a6fca20763fe20c3429d765a04bd7f5293481ba
Shan <perera.alc2000@gmail.com>
2023-10-31 21:04:39 +0530

    order package functions stage 1 : done

--- html/Booking/BookingController.js
+++ html/Booking/BookingController.js
@@ -51,6 +51,7 @@ promise_hotel_list.then((response) => {
             hotelItem.on('click', function () {
                 $('#selected_hotel_name').text($(this).text());
                 const selectedHotel = JSON.parse($(this).attr('data-value'));
+                // console.log(selectedHotel)
                 $('#hotel_list_combobox').val(JSON.stringify(selectedHotel));
                 document.getElementById("hotel_list_combobox").innerHTML = selectedHotel.id;
 
@@ -68,3 +69,97 @@ promise_hotel_list.then((response) => {
 });
 
 // -------------------------------------------------------------------------------------------------------
+let btnList = document.getElementsByClassName("select-option-btn");
+let cartTableBody = document.getElementById("selected_options_table_body"); // Assuming you have a cart table to append rows to
+
+function isRoomExists(id) {
+
+    const tableBody = document.getElementById("selected_option_table_body");
+    const rows = tableBody.getElementsByTagName("tr");
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const roomIdCell = row.querySelector(".col-room-id");
+        if (roomIdCell) {
+            if (id == roomIdCell.textContent.trim()) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+for (let i = 0; i < btnList.length; i++) {
+    btnList[i].addEventListener("click", function (e) {
+        e.preventDefault();
+        const selectedHotel = JSON.parse($('#hotel_list_combobox').val());
+        if (selectedHotel.options_list != null && selectedHotel.options_list.length > i) {
+            let selectedOption = selectedHotel.options_list[i];
+            if (!isRoomExists(selectedOption.id)) {
+
+                let hotel_id_cell = document.createElement('td');
+                hotel_id_cell.textContent = selectedHotel.id;
+                hotel_id_cell.className = 'col-hotel-id';
+
+                let hotel_name_cell = document.createElement('td');
+                hotel_name_cell.textContent = selectedHotel.name;
+                hotel_name_cell.className = 'col-hotel-name';
+
+                let room_description_cell = document.createElement('td');
+                room_description_cell.textContent = selectedOption.description;
+                room_description_cell.className = 'col-room-description';
+
+                let room_id_cell = document.createElement('td');
+                room_id_cell.textContent = selectedOption.id;
+                room_id_cell.className = 'col-room-id';
+
+                let option_price_cell = document.createElement('td');
+                option_price_cell.textContent = selectedOption.price;
+                option_price_cell.className = 'col-option-price';
+
+                let room_qty_input = document.createElement('input');
+                room_qty_input.classList.add('w-50');
+                room_qty_input.classList.add('form-control');
+                room_qty_input.setAttribute('type', 'number');
+                room_qty_input.setAttribute('min', '1');
+
+                let price_cell = document.createElement('td');
+
+                room_qty_input.addEventListener('input', function () {
+                    let quantity = parseInt(room_qty_input.value);
+                    let price = selectedOption.price;
+
+                    // Check if quantity is a valid number
+                    if (!isNaN(quantity)) {
+                        let total = price * quantity;
+                        price_cell.textContent = total;
+                    } else {
+                        price_cell.textContent = ''; // Clear the total if the input is not a valid number
+                    }
+                });
+
+                let room_qty_cell = document.createElement('td');
+                room_qty_cell.appendChild(room_qty_input);
+
+                let cart_row = document.createElement('tr');
+                cart_row.appendChild(hotel_id_cell);
+                cart_row.appendChild(hotel_name_cell);
+
+                cart_row.appendChild(room_id_cell);
+                cart_row.appendChild(room_description_cell);
+                cart_row.appendChild(option_price_cell);
+                cart_row.appendChild(room_qty_cell);
+                cart_row.appendChild(price_cell);
+
+                document.getElementById("selected_option_table_body").appendChild(cart_row);
+            } else {
+                Swal.fire('Warning', 'This room is already selected !', 'warning');
+            }
+        } else {
+            Swal.fire(
+                'Error!',
+                'No room found !'
+            );
+        }
+    });
+}
+//-------------------------------------------------------------------------------------------------------
